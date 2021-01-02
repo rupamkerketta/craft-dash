e7c31f8076178b5c953dd8c04b98b0d04569b107
Rupam Kerketta <kerkettarudolf@gmail.com>
2021-01-02 10:26:42 +0530

    :construction: Google Social Login in production

--- src/auth/social-login.js
+++ src/auth/social-login.js
@@ -1,8 +1,6 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth2').Strategy
 
-const keys = require('./keys')
-
 // User Model
 const User = require('../models/user')
 
@@ -18,8 +16,14 @@ passport.deserializeUser((id, done) => {
 passport.use(
 	new GoogleStrategy(
 		{
-			clientID: keys.google.clientID,
-			clientSecret: keys.google.clientSecret,
+			clientID:
+				process.env.NODE_ENV === 'PRODUCTION'
+					? process.env.GOOGLE_CLIENT_ID
+					: process.env.GOOGLE_CLIENT_ID_LOCAL,
+			clientSecret:
+				process.env.NODE_ENV === 'PRODUCTION'
+					? process.env.GOOGLE_CLIENT_SECRET
+					: process.env.GOOGLE_CLIENT_SECRET_LOCAL,
 			callbackURL: '/auth/google/callback'
 		},
 		(accessToken, refreshToken, profile, done) => {

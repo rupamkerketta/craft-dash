64b4b8d648c0f6f47ef51933adf98411a8d46ec3
Rupam Kerketta <kerkettarudolf@gmail.com>
2021-01-07 18:58:05 +0530

    :bug: Redirect (fix)

--- client/src/App.js
+++ client/src/App.js
@@ -15,9 +15,9 @@ import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
 	useEffect(() => {
-		if (!(window.location.hostname === 'www.craftdash.xyz')) {
-			window.location.hostname = 'https://www.craftdash.xyz'
-		}
+		// if (!(window.location.hostname === 'www.craftdash.xyz')) {
+		// 	window.location.hostname = 'https://www.craftdash.xyz'
+		// }
 		store.dispatch(loadUser())
 		console.log('[CraftDash]')
 	}, [])

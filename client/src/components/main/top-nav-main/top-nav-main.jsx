9137ec7608f8f64d435211b1ab8c9b2fcf4b6450
Vishnu Jayakumar <vjk10barca@gmail.com>
2021-02-22 17:38:20 +0530

    :sparkles: :lipstick: :construction: Craft Dash Cloud

--- client/src/components/main/top-nav-main/top-nav-main.jsx
+++ client/src/components/main/top-nav-main/top-nav-main.jsx
@@ -1,58 +1,65 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import './top-nav-main.scss'
+import React from "react";
+import { connect } from "react-redux";
+import "./top-nav-main.scss";
 
 // Brand Logo
-import BrandLogo from '../../brand-logo/brand-logo'
+import BrandLogo from "../../brand-logo/brand-logo";
+import CloudAddLogo from "../../../img/CloudAddLogo.png";
+import { Link } from "react-router-dom";
 
 // Video Actions
 import {
-	enableFullMode,
-	disableFullMode
-} from '../../../redux/video/videoActions'
+  enableFullMode,
+  disableFullMode,
+} from "../../../redux/video/videoActions";
 
 // Expand Button
-import ExpandButtonActive from '../../../img/expand-btn-inactive.png'
-import ExpandButtonInactive from '../../../img/expand-btn-active-min.png'
+import ExpandButtonActive from "../../../img/expand-btn-inactive.png";
+import ExpandButtonInactive from "../../../img/expand-btn-active-min.png";
 
 function TopNav({ videoFullMode, enableFullMode, disableFullMode }) {
-	const videoToggleHandler = () => {
-		if (videoFullMode) {
-			disableFullMode()
-		} else {
-			enableFullMode()
-		}
-	}
+  const videoToggleHandler = () => {
+    if (videoFullMode) {
+      disableFullMode();
+    } else {
+      enableFullMode();
+    }
+  };
 
-	return (
-		<div className='top-nav-main'>
-			<div className='expand-btn-wrapper'>
-				<img
-					className='expand-btn'
-					src={videoFullMode ? ExpandButtonActive : ExpandButtonInactive}
-					alt='Expand Video'
-					onClick={videoToggleHandler}
-				/>
-			</div>
-			<div className='brand-logo-wrapper'>
-				<BrandLogo
-					logoStyles={{ width: '20px' }}
-					fontStyles={{ fontSize: '1.2em', marginLeft: '10px' }}
-				/>
-			</div>
-		</div>
-	)
+  return (
+    <div className="top-nav-main">
+      <div className="expand-btn-wrapper">
+        <img
+          className="expand-btn"
+          src={videoFullMode ? ExpandButtonActive : ExpandButtonInactive}
+          alt="Expand Video"
+          onClick={videoToggleHandler}
+        />
+      </div>
+      <div className="brand-logo-wrapper">
+        <BrandLogo
+          logoStyles={{ width: "20px" }}
+          fontStyles={{ fontSize: "1.2em", marginLeft: "10px" }}
+        />
+      </div>
+      {/* <Link to="/craft-dash-cloud"> */}
+        <div className="upload-file-wrapper">
+          <img src={CloudAddLogo} className="upload-file-button" />
+        </div>
+      {/* </Link> */}
+    </div>
+  );
 }
 
 const mapStateToProps = (state) => {
-	return {
-		videoFullMode: state.video.videoFullMode
-	}
-}
+  return {
+    videoFullMode: state.video.videoFullMode,
+  };
+};
 
 const mapDispatchToProps = {
-	enableFullMode,
-	disableFullMode
-}
+  enableFullMode,
+  disableFullMode,
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(TopNav)
+export default connect(mapStateToProps, mapDispatchToProps)(TopNav);
